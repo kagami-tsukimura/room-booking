75472b43da95e8fd8113bc9898d0591933d90e0e
Kagami <shimura.kyohei7617@gmail.com>
2023-09-12 11:52:43 +0900

    update: sidebar get user

--- streamlit_util/users_page.py
+++ streamlit_util/users_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-from streamlit_util.get_response import convert_users_to_df, get_users
+from streamlit_util.get_response import convert_users_to_df, get_user, get_users
 from streamlit_util.post_response import show_response
 from streamlit_util.session import session_check
 
@@ -12,6 +12,13 @@ def show_user_page(page_title):
     if users:
         st.write("#### ユーザー一覧")
         st.table(df_users)
+
+        st.sidebar.title("ユーザー更新")
+        user_id = st.sidebar.selectbox("ユーザーID", df_users["ユーザーID"], key="delete")
+        user_name: str = st.sidebar.text_input(
+            "ユーザー名", value=get_user(user_id)["user_name"], max_chars=12
+        )
+
     else:
         st.info("ユーザーを登録してください。", icon="ℹ️")
 
