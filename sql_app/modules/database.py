381de67d8f925b9f053ec289197d4caa340d6b2d
Kagami <shimura.kyohei7617@gmail.com>
2023-09-15 21:05:31 +0900

    fix: sqlite

--- sql_app/modules/database.py
+++ sql_app/modules/database.py
@@ -1,11 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///../sql_app.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+os.chmod("./", 0o777)
+os.chmod("./sql_app.db", 0o777)
