cec8fd0522596c071837c19ff88469af54e6b5dd
Kagami <shimura.kyohei7617@gmail.com>
2023-09-16 00:03:05 +0900

    chore: test

--- sql_app/modules/database.py
+++ sql_app/modules/database.py
@@ -1,5 +1,3 @@
-import subprocess
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -11,7 +9,3 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-
-subprocess.run(["ls", "-l"])
-subprocess.run(["ls", "-l", "modules"])
