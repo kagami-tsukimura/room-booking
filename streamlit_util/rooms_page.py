7d59097d72c15e6877a2c11dbf80b09a60f0c67a
Kagami <shimura.kyohei7617@gmail.com>
2023-09-16 10:29:12 +0900

    fix: room booking

--- streamlit_util/rooms_page.py
+++ streamlit_util/rooms_page.py
@@ -42,18 +42,10 @@ def create_room(df_rooms, page_title):
         submit_button = st.form_submit_button(label="登録")
 
     if submit_button:
-        validation_error = validate_same_room(df_rooms, room_name)
-        if validation_error:
-            st.error(validation_error, icon="🔥")
+        if not room_name:
+            st.error("会議室名を入力してください。", icon="🔥")
         else:
             show_response(page_title, data)
-
-
-def validate_same_room(df_rooms, room_name):
-    if not room_name:
-        return "会議室名を入力してください。"
-    # if not df_rooms.empty and room_name in df_rooms["会議室名"].values:
-    #     return f"{room_name}は登録済みです。別の会議室名に変更してください。"
 
 
 def update_room(df_rooms, page_title):
@@ -64,10 +56,7 @@ def update_room(df_rooms, page_title):
         update_button = st.form_submit_button("変更")
 
     if update_button:
-        validation_error = validate_same_room(df_rooms, room_name)
-        error_message = validate_update_room(
-            validation_error, room_id, capacity, room_name
-        )
+        error_message = validate_update_room(room_id, capacity, room_name)
         if error_message:
             st.error(error_message, icon="🔥")
         else:
@@ -79,9 +68,9 @@ def update_room(df_rooms, page_title):
             update_response(page_title, room_id, payload)
 
 
-def validate_update_room(validation_error, room_id, capacity, room_name):
-    if validation_error:
-        return validation_error
+def validate_update_room(room_id, capacity, room_name):
+    if not room_name:
+        return "会議室名を入力してください。"
     bookings = get_bookings_filtered_room(room_id)
     booked_num = [booking.get("booked_num") for booking in bookings]
     sort_booked_num = sorted(booked_num, reverse=True)
@@ -90,8 +79,7 @@ def validate_update_room(validation_error, room_id, capacity, room_name):
     )
     if capacity_over_booked_num is None:
         return
-    validation_capacity = f"{room_name}は既に{capacity_over_booked_num}名の予約がされています。定員を{capacity_over_booked_num}名以上にしてください。"
-    return validation_capacity
+    return f"{room_name}は既に{capacity_over_booked_num}名の予約がされています。定員を{capacity_over_booked_num}名以上にしてください。"
 
 
 def delete_room(df_rooms, page_title):
